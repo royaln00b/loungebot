fe8173e99f16af05249e1b971c1d11414bc375d5
royaln00b <37339403+royaln00b@users.noreply.github.com>
2018-06-12 15:45:18 +0100

    Update loungebot.py

--- loungebot.py
+++ loungebot.py
@@ -148,13 +148,13 @@ keyz = "q"
 async def help(ctx,*,command=None):
     if command == None:
         await bot.add_reaction(message = ctx.message, emoji = "✅")
-        embed1 = discord.Embed(title = "Chill Bot" , description = "Hey! Welcome the the -help handbook.\nHere you can find out all the commands you can use in Avvie's Chill Server\nEnjoy your stay :)\nTo look at how to use a command do -help [command] to see how it works!\n\nIf you have any questions regarding the bot/commands, PM Royalnoob\nThanks to these amazing people the bot would not be as amazing as it is today!\nRoyalnoob\nCallum\nSesmic\nAvvie",colour = 0xFF0000)
-        embed2 = discord.Embed(title = "Utility    Page 1 / 6" , description = "-status - Check if the bot is online\n-ping - Check the relay speed of the bot\n-uptime - See how long the bot has been online for\n-date - Show the date\n-profile - Shows information about the member / yourself if left blank\n-achievemnets - Show all available achievements",colour = 0xFF0000)
+        embed1 = discord.Embed(title = "Chill Bot" , description = "Hey! Welcome the the -help handbook.\nHere you can find out all the commands you can use in the server!\nEnjoy your stay :)\nTo look at how to use a command do -help [command] to see how it works!\n\nIf you have any questions regarding the bot/commands, PM Royalnoob\nThanks to these amazing people the bot would not be as amazing as it is today!\nRoyalnoob\nCallum\nSesmic",colour = 0xFF0000)
+        embed2 = discord.Embed(title = "Utility    Page 1 / 6" , description = "-status - Check if the bot is online\n-ping - Check the relay speed of the bot\n-uptime - See how long the bot has been online for\n-date - Show the date\n-profile - Shows information about the member / yourself if left blank",colour = 0xFF0000)
         embed3 = discord.Embed(title = "Actions    Page 2 / 6" , description = "-hug - Hug a member\n-kiss - Kiss a member\n-lick - Lick another member\n-slap - Slap another member\n-pat - Pat another member\n-cuddle - Cuddle woth another member\n-pout - To pout\n-bite - Bite another member",colour = 0xFF0000)
-        embed4 = discord.Embed(title = "Chill Currency    Page 3 / 6" , description = "-balance/-b - Check your balance\n-leaderboard/-l - Check who the top 5 richest people on the server are\n-daily - Collect your daily Chill Coins\n-level - Check what level you are\n-xp - Check how much xp you have\n-donate - Donate your coins to another member\n-shop - See what you can buy with your Chill Coins",colour = 0xFF0000)
+        embed4 = discord.Embed(title = "Chill Currency    Page 3 / 6" , description = "-balance/-b - Check your balance\n-leaderboard/-l - Check who the top 5 richest people on the server are\n-daily - Collect your daily Chill Coins\n-level - Check what level you are\n-xp - Check how much xp you have\n-donate - Donate your coins to another member",colour = 0xFF0000)
         embed5 = discord.Embed(title = "Games    Page 4 / 6" , description = "-8ball - Ask the Chill Bot a yes or no question\n-guess - Guess a random number between 1 and 10\n-flip - Flip a coin\n-rps - Rock paper scissors\n-coinflip - Flip a coin and earn chill currency\n-slot - (VIP only) Run a slot machine with a max prize of 10x your bet!\n-rtd - Roll The dice. Roll a 5 or 6 and you win!",colour = 0xFF0000)  
         embed6 = discord.Embed(title = "Self Assignable Roles    Page 5 / 6" , description = "-colors - Check what colors you can assign\n-colorme - Add yourself to a color\n-uncolorme - Remove yourself from a color",colour = 0xFF0000)
-        embed7 = discord.Embed(title = "Pings    Page 6 / 6" , description = "-mug - Pings the maker of Chill bot!\n-siccwan - Pings the server owner\n-neko - Pings the chaotic neko\n-yiff - Pings our local furry\n-urmomgay - Pings Callum\n-alwayswatching - Pings the big sister",colour = 0xFF0000)   
+        embed7 = discord.Embed(title = "Pings    Page 6 / 6" , description = "-mug - Pings the mug himself\n-yiff - Pings our local furry",colour = 0xFF0000)   
         message = await bot.send_message(ctx.message.author,embed=embed1)
         await bot.add_reaction(message = message, emoji = "▶")
         while keyz == "q":
@@ -226,9 +226,6 @@ async def help(ctx,*,command=None):
     elif command == "donate":
         await bot.add_reaction(message = ctx.message, emoji = "✅")
         await bot.send_message(ctx.message.author,"-donate [member] [amount]")
-    elif command == "shop":
-        await bot.add_reaction(message = ctx.message, emoji = "✅")
-        await bot.send_message(ctx.message.author,"-shop [no argument needed]")
     elif command == "8ball":
         await bot.add_reaction(message = ctx.message, emoji = "✅")
         await bot.send_message(ctx.message.author,"-8ball [question]")
@@ -247,42 +244,18 @@ async def help(ctx,*,command=None):
     elif command == "slot":
         await bot.add_reaction(message = ctx.message, emoji = "✅")
         await bot.send_message(ctx.message.author,"-slot [amount]")
-    elif command == "colors":
-        await bot.add_reaction(message = ctx.message, emoji = "✅")
-        await bot.send_message(ctx.message.author,"-colors [no argument needed]")
-    elif command == "colorme":
-        await bot.add_reaction(message = ctx.message, emoji = "✅")
-        await bot.send_message(ctx.message.author,"-colorme [color]")
-    elif command == "uncolorme":
-        await bot.add_reaction(message = ctx.message, emoji = "✅")
-        await bot.send_message(ctx.message.author,"-uncolorme [color]")
     elif command == "mug":
         await bot.add_reaction(message = ctx.message, emoji = "✅")
         await bot.send_message(ctx.message.author,"-mug [no argument needed]")
-    elif command == "siccwan":
-        await bot.add_reaction(message = ctx.message, emoji = "✅")
-        await bot.send_message(ctx.message.author,"-siccwan [no argument needed]")
-    elif command == "neko":
-        await bot.add_reaction(message = ctx.message, emoji = "✅")
-        await bot.send_message(ctx.message.author,"-neko [no argument needed]")
     elif command == "yiff":
         await bot.add_reaction(message = ctx.message, emoji = "✅")
         await bot.send_message(ctx.message.author,"-yiff [no argument needed]")
-    elif command == "urmomgay":
-        await bot.add_reaction(message = ctx.message, emoji = "✅")
-        await bot.send_message(ctx.message.author,"-urmomgay [no argument needed]")
-    elif command == "alwayswatching":
-        await bot.add_reaction(message = ctx.message, emoji = "✅")
-        await bot.send_message(ctx.message.author,"-alwayswatching [no argument needed]")
     elif command == "date":
         await bot.add_reaction(message = ctx.message, emoji = "✅")
         await bot.send_message(ctx.message.author,"-date [no argument needed]")
     elif command == "rtd":
         await bot.add_reaction(message = ctx.message, emoji = "✅")
         await bot.send_message(ctx.message.author,"-rtd [bet]")
-    elif command == "achievements":
-        await bot.add_reaction(message = ctx.message, emoji = "✅")
-        await bot.send_message(ctx.message.author,"-achievements [no argument needed]")
     else:
         await bot.add_reaction(message = ctx.message, emoji = "❌")
         
