f03b3202f26e65243e7233af190827d3b1a02eef
royaln00b <37339403+royaln00b@users.noreply.github.com>
2018-06-18 09:35:27 +0100

    Update loungebot.py

--- loungebot.py
+++ loungebot.py
@@ -26,7 +26,7 @@ t = datetime.datetime.now()
 async def yt(ctx,*,url):
 	author = ctx.message.author
 	voice_channel = author.voice_channel
-	vc = await client.join_voice_channel(voice_channel)
+	vc = await bot.join_voice_channel(voice_channel)
 	player = await vc.create_ytdl_player(url)
 	player.start()
     
